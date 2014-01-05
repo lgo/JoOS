b39ced8a3a45a7e1cfd71a5beb90d98c4da606eb
Joey Pereira <joey@joeypereira.com>
2014-01-04 23:26:49 -0500

    a bit of comment cleaning

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -32,12 +32,14 @@ enum vga_color
 	COLOR_LIGHT_BROWN = 14,
 	COLOR_WHITE = 15,
 };
- 
+
+/* Combining colors */
 uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
  
+/* Write to the VGA memory*/
 uint16_t make_vgaentry(char c, uint8_t color)
 {
 	uint16_t c16 = c;
@@ -45,6 +47,7 @@ uint16_t make_vgaentry(char c, uint8_t color)
 	return c16 | color16 << 8;
 }
  
+/* Return the length of a string */
 size_t strlen(const char* str)
 {
 	size_t ret = 0;
@@ -60,7 +63,8 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
+
+/* init kernal terminal */
 void terminal_initialize()
 {
 	terminal_row = 0;
@@ -76,18 +80,21 @@ void terminal_initialize()
 		}
 	}
 }
- 
+
+/* set kernal background color */
 void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
  
+/* push a character and color to screen grid location x,y */
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = make_vgaentry(c, color);
 }
- 
+
+/* push a character to the screen */
 void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
@@ -101,12 +108,14 @@ void terminal_putchar(char c)
 	}
 }
 
+/* move the position of the writer to a new line  */
 void terminal_newline()
 {
     ++terminal_row;
     terminal_column = 0;
 }
- 
+
+/* write a string to the terminal */
 void terminal_writestring(const char* data)
 {
 	size_t datalen = strlen(data);
@@ -121,11 +130,11 @@ void terminal_writestring(const char* data)
                 }
         }
 }
- 
+
+/* kernel entry point */
 void kernel_main()
 {
 	terminal_initialize();
-	/* Since there is no support for newlines in terminal_putchar yet, \n will
-	   produce some VGA specific character instead. This is normal. */
-	terminal_writestring("Hello, kernel's World!\nSss");
+	// our little bit of hello world!
+        terminal_writestring("Hello, kernel's World!\nSss");
 }
