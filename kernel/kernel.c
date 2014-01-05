3a1276317c05888b6871059e9d084e7801ff9fbc
Joey Pereira <joey@joeypereira.com>
2014-01-05 15:26:40 -0500

    functional vertical scrolling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -58,11 +58,12 @@ size_t strlen(const char* str)
  
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 24;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
+uint16_t empty;
 
 /* init kernal terminal */
 void terminal_initialize()
@@ -71,12 +72,13 @@ void terminal_initialize()
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
+  empty = make_vgaentry(' ', terminal_color);
 	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
 	{
 		for ( size_t x = 0; x < VGA_WIDTH; x++ )
 		{
 			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = make_vgaentry('+', terminal_color);
+			terminal_buffer[index] = empty;
 		}
 	}
 }
@@ -86,7 +88,46 @@ void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
+
+/* init the line  */
+void terminal_initline(size_t line)
+{
+  for (size_t x = 0; x < VGA_WIDTH; x++)
+  {
+    const size_t index = line * VGA_WIDTH + x;
+    terminal_buffer[index] = empty;
+  }
+}
+
+/* move all of the characters in the buffer upwards  */
+void terminal_scrollup()
+{
+  for (size_t y = 1; y < VGA_HEIGHT; y++)
+  {
+    for (size_t x = 0; x < VGA_WIDTH; x++)
+    {
+      const size_t index = y * VGA_WIDTH + x;
+      const size_t new_index = index - VGA_WIDTH;
+      terminal_buffer[new_index] = terminal_buffer[index];
+    }
+  }
+  terminal_initline(VGA_WIDTH);
+}
+
+/* reposition the writing placement */
+void terminal_resetline()
+{
+  if (++terminal_column == VGA_WIDTH)
+  {
+    terminal_column = 0;
+    if (++terminal_row == VGA_HEIGHT)
+    {
+      terminal_scrollup();
+      --terminal_row;
+    }
+  }
+} 
+
 /* push a character and color to screen grid location x,y */
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
@@ -97,22 +138,15 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 /* push a character to the screen */
 void terminal_putchar(char c)
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if ( ++terminal_column == VGA_WIDTH )
-	{
-		terminal_column = 0;
-		if ( ++terminal_row == VGA_HEIGHT )
-		{
-			terminal_row = 0;
-		}
-	}
+  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+  terminal_resetline();
 }
 
 /* move the position of the writer to a new line  */
 void terminal_newline()
 {
-    ++terminal_row;
-    terminal_column = 0;
+    terminal_column = VGA_WIDTH - 1; //bit of a hack to get everything to cooperate the way it is
+    terminal_resetline();
 }
 
 /* write a string to the terminal */
@@ -137,4 +171,5 @@ void kernel_main()
 	terminal_initialize();
 	// our little bit of hello world!
         terminal_writestring("Hello, kernel's World!\nSss");
+        terminal_writestring("Even more\n\n\n hmm \n\n\n\n\n\n\n yes...\n\n\n\n\n\n\n\n\nMORE\n\n\n\nHmm\nYes :)");
 }
